5e3f8190afe8e131097a94746651f6eddfc9164a
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-04-16 08:02:14 -0400

    day

---
+++ Github Application Updater/Debug Console.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Github_Application_Updater {
+    /// <summary>
+    /// Interaction logic for Debug_Console.xaml
+    /// </summary>
+    public partial class DebugConsole : Window {
+        public DebugConsole() {
+            InitializeComponent();
+        }
+
+        public void Error(string text) => Add($"[{DateTime.Now.ToString("G")}] {text}", Brushes.Red);
+
+        public void Warn(string text) => Add($"[{DateTime.Now.ToString("G")}] {text}", Brushes.Goldenrod);
+
+        public void Log(string text) => Add($"[{DateTime.Now.ToString("G")}] {text}", Brushes.Black);
+
+        private void Add(string message, Brush color) { 
+            Console.Items.Add(new TextBlock {
+                    Text = message,
+                    Foreground = color
+            });
+        }
+    }
+}
--- Github Application Updater/MainWindow.xaml.cs
+++ Github Application Updater/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,39 @@ namespace Github_Application_Updater {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        DebugConsole Console;
+
         public MainWindow() {
             InitializeComponent();
+
+            Console = new DebugConsole();
+
+            Closing += MainWindowClosing;
+
+            #if DEBUG
+                Console.Show();
+            #endif
+
+            Console.Error("ERROR");
+            Console.Warn("WARNING");
+            Console.Log("LOG");
+        }
+
+        public void MainWindowClosing(object sender, CancelEventArgs args) {
+            Console.Close();
+        }
+
+        private void Add_MouseEnter(object sender, MouseEventArgs e) {
+            Brush hover = new SolidColorBrush(Color.FromRgb(42, 42, 42));
+            Add.Foreground = hover;
+            AddPlus.Fill = hover;
+        }
+
+        private void Add_MouseLeave(object sender, MouseEventArgs e) {
+            Brush hover = Brushes.Black;
+            Add.Foreground = hover;
+            AddPlus.Fill = hover;
         }
     }
 }
